cdbb4bde00be9e5825beb7b9c80c13c7cbf6c3f9
Taewon Hwang <taewonhwang7@gmail.com>
2024-11-26 15:55:39 -0500

    dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import LandingPage from './pages/landingPage'
 import LoginPage from './pages/loginPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashboardPage from './pages/dashboardPage';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        
       </Routes>
     </Router>
   );
