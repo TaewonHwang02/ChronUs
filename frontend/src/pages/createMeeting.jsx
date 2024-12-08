28041d311b09f726b42deaa8596d25c632e16060
danana0304 <danal0304@gmail.com>
2024-12-07 19:07:06 -0500

    createmeeting

---
+++ frontend/src/pages/createMeeting.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const createMeeting = () => {
+    return (
+        <div className="relative w-full h-screen  bg-steel_blue">
+            {/* <div className="relative w-">
+
+            </div> */}
+        </div>
+    );
+};
+
+
+export default CreateMeeting;
