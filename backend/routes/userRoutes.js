1cee1643eff7ddbff5f520b10eb0442d2e13ca13
Taewon Hwang <taewonhwang7@gmail.com>
2024-12-14 17:07:10 -0500

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -21,13 +21,12 @@ router.post("/register", verifyFirebaseToken, async (req, res) => {
             user = new User({ uid, email, name });
             await user.save();
         } else {
-            console.log("User already exists:", user);
+
             return res.status(409).json({ message: "User already exists" });
         }
         res.status(200).json({ message: "User synced", user });
     } catch (error) {
-        console.error("Error syncing user:", error.message);
-        console.error("Stack trace:", error.stack); // Log full stack trace
+
         res.status(500).json({ message: "Failed to sync user with MongoDB" });
     }
 });
