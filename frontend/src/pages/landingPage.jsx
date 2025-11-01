0899495c5677b46d14790d58bea28095798ce143
danana0304 <danal0304@gmail.com>
2025-11-02 07:30:16 +0900

    Landing Page functionality

--- frontend/src/pages/landingPage.jsx
+++ frontend/src/pages/landingPage.jsx
@@ -8,14 +8,14 @@ const LandingPage = ({ darkMode, setDarkMode }) => {
   return (
     <div
       className="relative w-full h-screen bg-primary flex tb:flex-row items-center justify-center tb:justify-start overflow-hidden
-    bg-center bg-[length:200%_auto] bg-no-repeat z-[-2]
+    bg-center bg-[length:200%_auto] bg-no-repeat z-0
     "
     >
       <div
-        className="absolute inset-0 bg-center bg-[length:200%_auto] bg-no-repeat 
-    bg-[url('/src/assets/lightBg.svg')] ph:bg-[url('/src/assets/bg-light-desktop.jpg')]
-    dark:bg-[url('/src/assets/darkBg.svg')] dark:ph:bg-[url('/src/assets/bg-dark-desktop.jpg')]
-    animate-float-slow z-[-1]"
+        className="absolute inset-0 bg-center bg-[length:200%_auto] ph:bg-[length:120%_auto] bg-no-repeat 
+    bg-[url('/src/assets/lightBg.svg')] ph:bg-[url('/src/assets/bg-light-desktop.svg')]
+    dark:bg-[url('/src/assets/darkBg.svg')] dark:ph:bg-[url('/src/assets/bg-dark-desktop.svg')]
+    animate-float-slow z-0"
       ></div>
       <div className="absolute w-full h-screen backdrop-blur-xs bg-gradient-to-b from-slate-900 to-transparent"></div>
 
@@ -30,10 +30,10 @@ const LandingPage = ({ darkMode, setDarkMode }) => {
           <h2 className="text-lg tb:text-2xl font-poppins mb-2 py-2">with</h2>
           <h1 className="text-6xl tb:text-6xl font-kulim">
             <span className="text-white font-normal">Chron</span>
-            <span className="text-selective_yellow text-6xl tb:text-6xl uppercase">
+            <span className="text-tertiary text-6xl tb:text-6xl uppercase">
               u
             </span>
-            <span className="text-selective_yellow">s</span>
+            <span className="text-tertiary">s</span>
           </h1>
         </div>
 
@@ -47,28 +47,6 @@ const LandingPage = ({ darkMode, setDarkMode }) => {
             <Link to="/createMeeting">
               <Button text="Start Hosting"></Button>
             </Link>
-          </div>
-        </div>
-
-        <button
-          onClick={() => setShowToggle(!showToggle)}
-          className="absolute bottom-20 left-1/2 transform -translate-x-1/2 p-3 bg-blue-500 text-white rounded"
-        >
-          Toggle Dark/Light
-        </button>
-
-        <div className="flex space-x-2 justify-center items-center z-0">
-          <div
-            onClick={() => setDarkMode(false)}
-            className="p-4 bg-slate-300 rounded-sm"
-          >
-            <div></div>
-          </div>
-          <div
-            onClick={() => setDarkMode(true)}
-            className="p-4 bg-slate-300 rounded-sm"
-          >
-            Dark
           </div>
         </div>
 
@@ -87,6 +65,66 @@ const LandingPage = ({ darkMode, setDarkMode }) => {
           }
         `}
         </style>
+      </div>
+
+      {/* Option Panel */}
+      <div className="absolute bottom-0 w-full transform flex flex-col items-end z-10 font-poppins text-sm text-white">
+        <div
+          className={`cursor-pointer bg-tertiary text-white
+      flex flex-col items-center justify-center
+      transition-all duration-300 ease-in-out
+      ${
+        showToggle
+          ? "w-full h-1/4 rounded-lg p-2"
+          : "mb-8 mr-6 w-12 h-12 rounded-full p-0"
+      }
+    `}
+        >
+          {!showToggle && (
+            <span onClick={() => setShowToggle(true)} className="text-2xl">
+              +
+            </span>
+          )}
+
+          {/* Expanded panel */}
+          {showToggle && (
+            <div className="flex flex-row w-full h-full justify-center relative">
+              <div className="flex flex-col items-center">
+                {/* Theme selector */}
+                <div className="flex flex-row space-x-2 p-3">
+                  <div
+                    onClick={() => setDarkMode(false)}
+                    className={`p-1 rounded-sm cursor-pointer flex flex-row space-x-1 transition-colors duration-300
+                ${!darkMode ? "bg-yellow-700" : "bg-slate-200"}`}
+                  >
+                    <div className="rounded-sm w-6 h-8 bg-selective_blue"></div>
+                    <div className="rounded-sm w-6 h-8 bg-white"></div>
+                    <div className="rounded-sm w-6 h-8 bg-tertiary"></div>
+                  </div>
+
+                  <div
+                    onClick={() => setDarkMode(true)}
+                    className={`p-1 rounded-sm cursor-pointer flex flex-row space-x-1 transition-colors duration-300
+                ${darkMode ? "bg-dark" : "bg-slate-200"}`}
+                  >
+                    <div className="rounded-sm w-6 h-8 bg-dark"></div>
+                    <div className="rounded-sm w-6 h-8 bg-grey"></div>
+                    <div className="rounded-sm w-6 h-8 bg-tertiary"></div>
+                  </div>
+                </div>
+
+                <span className="mt-2 text-white">Theme</span>
+              </div>
+
+              <div
+                onClick={() => setShowToggle(false)}
+                className="absolute top-0 right-0 mb-2 px-2 py-1 !border-0 text-white rounded-full text-xl cursor-pointer"
+              >
+                -
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
