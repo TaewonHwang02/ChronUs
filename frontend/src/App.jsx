01bbbe961e7ecbc6539798870e85dd9d9c92b54a
Taewon Hwang <taewonhwang7@gmail.com>
2025-11-01 02:03:48 -0400

    removed routing links for deleted pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/landingPage";
-// import LoginPage from "./pages/loginPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import DashboardPage from './pages/dashboardPage';
-// import RegisterPage from "./pages/registerPage";
 import SchedulingMainPage from "./pages/SchedulingMain";
-import LinkRetrievalPage from "./pages/linkRetrievalPage";
 import CreateMeeting from "./pages/createMeeting";
 import LinkPage from "./pages/linkPage";
 
@@ -33,14 +29,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-        {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
-        {/* <Route path="/register" element={<RegisterPage />} /> */}
+      
         <Route
           path="/schedulingmain/:meetingLink"
           element={<SchedulingMainPage />}
         />
-        <Route path="/linkretrieval" element={<LinkRetrievalPage />} />
         <Route path="/createMeeting" element={<CreateMeeting />} />
         <Route path="/join/:meetingLink" element={<LinkPage />} />
       </Routes>
